ba041b9266bf23f3ff8d947db09436001130ce1d
Gubaydullin Nurislam <108585608+tonitaga@users.noreply.github.com>
2025-10-15 20:30:29 +0300

    Доработка run_server.bar

--- run_server.bat
+++ run_server.bat
@@ -1,10 +1,19 @@
-@echo off 
- cls 
- title StartUp 
- :hlds 
- echo (%time%) HLDS Started... 
- reg add "HKCU\Software\Valve\Steam\ActiveProcess" /v SteamClientDll /t REG_SZ /d "" /f 
- start /wait /high hlds.exe -autoupdate -console -game cstrike -secure -master -noipx +map de_dust2 +maxplayers 32 +port 27015 
- echo n| goto hlds 
- echo (%time%) HLDS Crashed, restarting... 
- goto hlds
+@echo off
+cls
+title StartUp
+
+:: Переход в папку cstrike/hfs и запуск hfs.exe
+cd /d "cstrike\hfs"
+echo (%time%) Starting HFS...
+start "HFS Server" hfs.exe
+cd /d ../..
+
+:: Ожидание немного для запуска HFS
+timeout /t 1 /nobreak >nul
+
+:hlds
+echo (%time%) HLDS Starting...
+reg add "HKCU\Software\Valve\Steam\ActiveProcess" /v SteamClientDll /t REG_SZ /d "" /f
+start /wait /high hlds.exe -autoupdate -console -game cstrike -secure -master -noipx +map de_dust2 +maxplayers 32 +port 27015
+echo (%time%) HLDS Crashed, restarting...
+goto hlds
